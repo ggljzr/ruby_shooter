92a4f47e3152008831f74b592b106e29c043078a
ggljzr <gogo.lejzr@gmail.com>
2017-11-09 11:43:12 +0100

    added cannon states

--- ruby_shooter/model/cannon.rb
+++ ruby_shooter/model/cannon.rb
@@ -1,14 +1,31 @@
-require_relative 'game_object'
 require_relative 'missile'
+
+class StateSingle
+    def fire(cannon)
+        cannon.state = StateDouble.new
+        [Missile.new(cannon.x, cannon.y, cannon.angle)]
+    end
+end
+
+class StateDouble
+    def fire(cannon)
+        cannon.state = StateSingle.new
+        r = Missile.new(cannon.x, cannon.y, cannon.angle - 0.2)
+        l = Missile.new(cannon.x, cannon.y, cannon.angle + 0.2)
+        [r, l]
+    end
+end
 
 class Cannon < GameObject
     attr_reader :angle
+    attr_accessor :state
 
     MOVE_STEP = 5
     AIM_STEP = 0.2
 
     def initialize(x = 0, y = 0)
         super(x, y)
+        @state = StateDouble.new
         @angle = 0
     end
 
@@ -29,6 +46,6 @@ class Cannon < GameObject
     end
 
     def fire
-        Missile.new(@x, @y, @angle)
+        @state.fire(self)
     end
 end
