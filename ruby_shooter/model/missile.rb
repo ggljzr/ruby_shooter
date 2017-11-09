51137ab34d569ad1e0d351a3ac7e328fe460686a
ggljzr <gogo.lejzr@gmail.com>
2017-11-09 10:58:29 +0100

    added missile move strategies

--- ruby_shooter/model/missile.rb
+++ ruby_shooter/model/missile.rb
@@ -1,16 +1,37 @@
 require_relative 'game_object'
 
-class Missile < GameObject
+class SimpleMove
 
     MISSILE_SPEED = 6
 
-    def initialize(x = 0, y = 0, angle = 2 * Math::PI)
+    def move(missile)
+        missile.x += MISSILE_SPEED * Math.cos(missile.angle)
+        missile.y += MISSILE_SPEED * Math.sin(missile.angle)
+    end
+end
+
+class RealMove
+
+    MISSILE_SPEED = 6
+    ANGEL_FALLOFF = 0.01
+
+    def move(missile)
+        missile.x += MISSILE_SPEED * Math.cos(missile.angle)
+        missile.y += MISSILE_SPEED * Math.sin(missile.angle)
+        missile.angle += ANGEL_FALLOFF
+    end
+end
+
+class Missile < GameObject
+    attr_accessor :x, :y, :angle
+
+    def initialize(x = 0, y = 0, angle = 2 * Math::PI, move_strategy = SimpleMove.new)
         super(x, y)
         @angle = angle
+        @move_strategy = move_strategy
     end
 
     def move
-        @x += MISSILE_SPEED * Math.cos(@angle)
-        @y += MISSILE_SPEED * Math.sin(@angle)
+        @move_strategy.move(self)
     end
 end
