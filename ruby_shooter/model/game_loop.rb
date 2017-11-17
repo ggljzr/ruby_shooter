c4b8f67757de76959fad8f68e3b3233d24ba67f1
ggljzr <gogo.lejzr@gmail.com>
2017-11-17 03:08:42 +0100

    added time tracking

--- ruby_shooter/model/game_loop.rb
+++ ruby_shooter/model/game_loop.rb
@@ -12,22 +12,25 @@ class GameLoop
     @running = false
     @clock = Rubygame::Clock.new
     @clock.target_framerate = TARGET_FRAMERATE
+    @clock.enable_tick_events
   end
 
   def run
     @running = true
 
     while @running
-      @clock.tick
+      t = @clock.tick.seconds
 
       @game_model.commands.each() { |c| c.execute if !c.executed }
 
       @game_model.spawn_enemy
 
+      @game_model.enemies.each { |e| e.move(t) }
+
       @game_model.missiles.each do |m|
         m.move
         @game_model.enemies.each do |e|
-          e.move(m)
+          e.move_away(m)
           @game_model.enemy_missile_collision(e, m) if e.get_collision(m)
         end
       end
