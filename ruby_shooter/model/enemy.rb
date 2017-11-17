5574540652953c81e84a88346b3b01c20198da48
ggljzr <gogo.lejzr@gmail.com>
2017-11-17 03:11:46 +0100

    reworked move strategies

--- ruby_shooter/model/enemy.rb
+++ ruby_shooter/model/enemy.rb
@@ -2,42 +2,48 @@ require_relative 'game_object'
 require_relative 'direction'
 
 class SimpleEnemyMove
-  def get_next_position(enemy, missile)
+  def get_next_position(enemy, t)
     Direction.new(enemy.x, enemy.y, 0)
   end
 end
 
 class RealEnemyMove
-  RAND_STEP = 1
-  SCARE_COEFICIENT = 2.5
-  SCARE_SPEED = 3
-  DISPERSION = 0.2
+  SPIN_RADIUS = 6 * Math::PI
 
-  def get_next_position(enemy, missile)
-    new_x = 0
-    new_y = 0
+  def initialize
+    @angle = 0
+  end
 
-    if enemy.get_collision_dist(missile) < enemy.r * SCARE_COEFICIENT
-      running_angle = missile.angle + rand(-DISPERSION..DISPERSION)
-      new_x = SCARE_SPEED * Math.cos(running_angle) + enemy.x
-      new_y = SCARE_SPEED * Math.sin(running_angle) + enemy.y
-    else
-      new_x += rand(-RAND_STEP..RAND_STEP) + enemy.x
-      new_y += rand(-RAND_STEP..RAND_STEP) + enemy.y
-    end
+  def get_next_position(enemy, t)
+    @angle += SPIN_RADIUS / 4 * t % SPIN_RADIUS
+    new_x = enemy.x + Math.cos(@angle)
+    new_y = enemy.y + Math.sin(@angle)
     Direction.new(new_x, new_y, 0)
   end
 end
 
 class Enemy < GameObject
+  RAND_STEP = 1
+  SCARE_COEFICIENT = 5
+  SCARE_SPEED = 3
+  DISPERSION = 0.2
+
   def initialize(x = 0, y = 0, move_strategy = SimpleEnemyMove.new)
     super(x, y)
     @move_strategy = move_strategy
   end
 
-  def move(missile)
-    new_position = @move_strategy.get_next_position(self, missile)
+  def move(t)
+    new_position = @move_strategy.get_next_position(self, t)
     @x = new_position.x
     @y = new_position.y
+  end
+
+  def move_away(missile)
+    if get_collision_dist(missile) < @r * SCARE_COEFICIENT
+      running_angle = missile.angle + rand(-DISPERSION..DISPERSION)
+      @x += SCARE_SPEED * Math.cos(running_angle)
+      @y += SCARE_SPEED * Math.sin(running_angle)
+    end
   end
 end
