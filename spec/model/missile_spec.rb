4ddc60960aa46154d47b34ed8c42d4755185695c
ggljzr <gogo.lejzr@gmail.com>
2017-11-24 05:55:01 +0100

    added tests for missile and model

---
+++ spec/model/missile_spec.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+require_relative '../../ruby_shooter/model/missile.rb'
+require_relative '../spec_helper'
+
+describe 'Missile' do
+    before(:each) do
+        #create missile on pos [0, 0] with 0 angle
+        @missile = Missile.new(0, 0, 0)
+    end
+
+    context 'Has expected methods' do
+        it 'has move method' do
+            expect(@missile).to respond_to(:move)
+        end
+    end
+
+    context 'Moving behavior' do
+        it 'falls slightly when moving (simple strategy)' do
+            old_y = @missile.y
+            @missile.move
+            expect(@missile.y).to be > old_y
+        end
+
+        it 'moves slightly back (real strategy)' do
+            real_missile = Missile.new(0, 0, 0, 0, RealMissileMove.new)
+            @missile.move
+            real_missile.move
+
+            expect(real_missile.x).to be < @missile.x
+        end
+
+        it 'moves faster with bigger force' do
+            force_missile = Missile.new(0, 0, 0, 5)
+            @missile.move
+            force_missile.move
+
+            expect(force_missile.x).to be > @missile.x
+        end
+    end
+end
